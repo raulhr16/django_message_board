b4f6d5e1079cae2d907f4a6719e786751a73472e
José Domingo Muñoz <josedom24@gmail.com>
2024-11-30 12:58:02 +0100

    Estílo

--- django_project/settings.py
+++ django_project/settings.py
@@ -116,4 +116,4 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage" 
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-CSRF_TRUSTED_ORIGINS = ["https://*.fly.dev"]  # new
+
