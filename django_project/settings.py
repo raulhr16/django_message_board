0543b4beff57a3fe968b58049abf6bee59a00759
José Domingo Muñoz Rodríguez <josedom24@gmail.com>
2024-11-30 13:17:33 +0100

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = "django-insecure-!i1taz@my6u5yr09vw75lg*qo=d-**lr_#!!^r5t9(0oo2&t35
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [ ]
 
 
 # Application definition
